625b3d7694f691a90b0a9a921e343ca93895ae06
oooo0oooo <oooo0oooo@aliyun.com>
2019-05-25 18:09:44 +0800

    Create Class HelloGit

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Lykan
+ * @create 2019-05-25 18:08
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git......");
+    }
+}
